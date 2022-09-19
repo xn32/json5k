63543a918843227eb3f10a5f57d2213d9f13f488
Tobias <xn32@posteo.net>
2022-09-19 22:27:38 +0200

    Apply the 'maven-publish' plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    `maven-publish`
     `java-library`
 }
 
@@ -20,6 +21,14 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("lib") {
+            from(components["java"])
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
