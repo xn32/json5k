6a817e34aa18a52977eedc9b2b06d61bd9a3c283
Tobias <xn32@posteo.net>
2022-10-15 20:14:33 +0200

    Add documentation for public API of the library

--- src/main/kotlin/io/github/xn32/json5k/Configuration.kt
+++ src/main/kotlin/io/github/xn32/json5k/Configuration.kt
@@ -2,16 +2,51 @@ package io.github.xn32.json5k
 
 import kotlinx.serialization.modules.SerializersModule
 
+/**
+ * Type-safe builder to configure the parameters of a custom [Json5] instance.
+ */
 @Suppress("MagicNumber")
 class ConfigBuilder {
+    /**
+     * Supply a custom [SerializersModule] that will be passed to the kotlinx.serialization framework.
+     */
     var serializersModule: SerializersModule? = null
 
+    /**
+     * Class discriminator to use for the serialization and deserialization of polymorphic types without
+     * a [ClassDiscriminator] annotation.
+     */
     var classDiscriminator: String = "type"
+
+    /**
+     * Encode default values from the data model when the object hierarchy to serialize does not overwrite them.
+     */
     var encodeDefaults: Boolean = false
 
+    /**
+     * Request the generation of human-readable output (spanning multiple lines). Without this option,
+     * compressed single-line output is generated. [SerialComment] annotations are ignored without this setting.
+     */
     var prettyPrint: Boolean = false
+
+    /**
+     * Number of spaces to use for indenting each hierarchy level (objects/arrays) in generated JSON5 output.
+     * This setting does only have an effect if the [prettyPrint] option is activated.
+     */
     var indentationWidth: Int = 4
+
+    /**
+     * Request names of serialized object members to be quoted even if the quotation marks could be dropped.
+     * This setting does only have an effect if the [prettyPrint] option is activated. If compressed single-line
+     * output is generated, member names will be quoted only when necessary (regardless of this option).
+     */
     var quoteMemberNames: Boolean = false
+
+    /**
+     * Use single quotes instead of double quotes when serializing strings and member names in human-readable
+     * output. This setting does only have an effect if the [prettyPrint] option is activated. If compressed
+     * single-line output is generated, double quotes will be used (regardless of this option).
+     */
     var useSingleQuotes: Boolean = false
 }
 
--- src/main/kotlin/io/github/xn32/json5k/Json5.kt
+++ src/main/kotlin/io/github/xn32/json5k/Json5.kt
@@ -18,31 +18,152 @@ import kotlinx.serialization.modules.EmptySerializersModule
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.serializer
 import java.io.ByteArrayOutputStream
+import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 
+/**
+ * Main interface to serialize a given object hierarchy to JSON5 or vice versa.
+ *
+ * The default class implementing this interface is available as the [companion object][Json5.Default].
+ * It can be used as shown in the following example:
+ * ```
+ * import io.github.xn32.json5k.Json5
+ * import kotlinx.serialization.encodeToString
+ *
+ * Json5.encodeToString(1000)
+ * ```
+ *
+ * A custom instance is obtained by calling the builder function and populating the
+ * associated [ConfigBuilder] object with the desired configuration options.
+ */
 interface Json5 : StringFormat {
+    /**
+     * This companion object is the default implementation of the [Json5] interface.
+     *
+     * It passes an empty `SerializersModule` to the kotlinx.serialization framework and
+     * uses `type` as the class discriminator for the serialization and deserialization of
+     * polymorphic types. During the serialization process, it does not encode default values
+     * and generates compressed output (without line breaks for better readability).
+     */
     companion object Default : Json5 by ConfigBuilder().toImpl()
 
-    fun <T> decodeFromStream(deserializer: DeserializationStrategy<T>, inputStream: InputStream): T
+    /**
+     * Serializes the given [value] using the provided [serializer] and returns the value as a string.
+     *
+     * If multi-line output is requested, the function will use UNIX-style line endings. In any case,
+     * a final newline character is *not* generated.
+     *
+     * @param serializer [SerializationStrategy] to use for the serialization.
+     * @param value The object hierarchy to serialize.
+     * @return The generated JSON5 output (using LF as line terminator).
+     */
+    override fun <T> encodeToString(serializer: SerializationStrategy<T>, value: T): String
+
+    /**
+     * Deserializes the JSON5 input in [string] using the given [deserializer] and returns
+     * an equivalent object hierarchy.
+     *
+     * @param deserializer [DeserializationStrategy] to use for the deserialization.
+     * @param string JSON5 input to deserialize into an object hierarchy.
+     * @throws ParsingError The provided input cannot be parsed.
+     * @throws DecodingError The provided input is incompatible with the data model.
+     * @return The created object hierarchy.
+     */
+    override fun <T> decodeFromString(deserializer: DeserializationStrategy<T>, string: String): T
+
+    /**
+     * Serializes the given [value] using the given [serializer] and writes the result to [outputStream].
+     *
+     * Data written to [outputStream] will have UTF-8 encoding. It is not closed upon completion of this operation.
+     *
+     * If multi-line output is requested, the function will use UNIX-style line endings. In any case,
+     * a final newline character is *not* generated.
+     *
+     * @param serializer [SerializationStrategy] to use for the serialization.
+     * @param value The object hierarchy to serialize.
+     * @param outputStream [OutputStream] to write the UTF-8 result to.
+     * @throws IOException An error occurred while writing to [outputStream].
+     */
     fun <T> encodeToStream(serializer: SerializationStrategy<T>, value: T, outputStream: OutputStream)
+
+    /**
+     * Deserializes the JSON5 input from [inputStream] using the given [deserializer] and
+     * returns an equivalent object hierarchy.
+     *
+     * Data read from the [InputStream] is expected to have UTF-8 encoding.
+     *
+     * @param deserializer [DeserializationStrategy] to use for the deserialization.
+     * @param inputStream UTF-8-encoded stream to read the JSON5 input from.
+     * @throws ParsingError The provided input cannot be parsed.
+     * @throws DecodingError The provided input is incompatible with the data model.
+     * @throws IOException An error occurred while reading from [inputStream].
+     * @return The created object hierarchy.
+     */
+    fun <T> decodeFromStream(deserializer: DeserializationStrategy<T>, inputStream: InputStream): T
 }
 
+/**
+ * Type-safe builder to create a custom [Json5] instance.
+ *
+ * To configure the returned instance (by specifying a custom [SerializersModule] or adjusting JSON5-specific
+ * settings), populate the [ConfigBuilder] object accordingly. The following example shows how to create and
+ * use a custom instance:
+ * ```
+ * import io.github.xn32.json5k.Json5
+ * import kotlinx.serialization.encodeToString
+ *
+ * val json5 = Json5 {
+ *     prettyPrint = true
+ * }
+ *
+ * json5.encodeToString(listOf(10, 20, 30))
+ * ```
+ *
+ * A detailed description of available settings can be found in the [ConfigBuilder] documentation.
+ */
 fun Json5(actions: ConfigBuilder.() -> Unit): Json5 {
     val builder = ConfigBuilder()
     builder.actions()
     return builder.toImpl()
 }
 
+/**
+ * Annotation to set the class discriminator of polymorphic base types.
+ *
+ * Apply this to a polymorphic base type (such as an interface) to override the global class
+ * discriminator set in the [ConfigBuilder] object.
+ */
 @OptIn(ExperimentalSerializationApi::class)
 @InheritableSerialInfo
 @Target(AnnotationTarget.CLASS)
-annotation class ClassDiscriminator(val discriminator: String)
+annotation class ClassDiscriminator(
+    /**
+     * Class discriminator name to use for this base type. This value becomes effective for both
+     * serialization and deserialization. It is the caller's responsibility to ensure that the passed
+     * value is a single-line JSON5 string.
+     */
+    val discriminator: String
+)
 
+/**
+ * Annotation to specify a comment that should be serialized to every JSON5 occurrence of a property.
+ *
+ * When this annotation is applied to a property and human-readable JSON5 output is generated,
+ * every member of a generated JSON5 object that represents this property will be preceded by
+ * a JSON5 representation of this comment.
+ */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
 @SerialInfo
-annotation class SerialComment(val value: String)
+annotation class SerialComment(
+    /**
+     * Comment to generate whenever the property is serialized into a JSON5 object. This value will be
+     * trimmed using the [String.trimIndent] function of Kotlin. Multi-line strings (separated by any
+     * of the JSON5 line terminators) are supported.
+     */
+    val value: String
+)
 
 private fun ConfigBuilder.toImpl(): Json5 {
     return Json5Impl(serializersModule ?: EmptySerializersModule(), toSettings())
@@ -83,9 +204,15 @@ private class Json5Impl(
     }
 }
 
+/**
+ * Convenience function that calls [Json5.decodeFromStream] with the default serializer for the type.
+ */
 inline fun <reified T> Json5.decodeFromStream(inputStream: InputStream): T =
     decodeFromStream(serializersModule.serializer(), inputStream)
 
+/**
+ * Convenience function that calls [Json5.encodeToStream] with the default serializer for the type.
+ */
 inline fun <reified T> Json5.encodeToStream(value: T, outputStream: OutputStream) =
     encodeToStream(serializersModule.serializer(), value, outputStream)
 
