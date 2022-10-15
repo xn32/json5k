9a3cbd0dc4f4a46d1e31dbf6b9b0aae54a092f1f
Tobias <xn32@posteo.net>
2022-10-15 21:57:56 +0200

    Generate an API documentation using Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URL
 
 plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    id("org.jetbrains.dokka") version "1.7.10"
     `maven-publish`
     `java-library`
 }
@@ -37,4 +40,45 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<GenerateModuleMetadata> {
     enabled = false
+}
+
+tasks.withType<DokkaTask>() {
+    val githubRepo = "https://github.com/xn32/json5k"
+    val footerMsg = "Find the <a href='$githubRepo'>json5k repository</a> on GitHub."
+
+    dokkaSourceSets {
+        configureEach {
+            externalDocumentationLink {
+                url.set(URL("https://kotlinlang.org/api/kotlinx.serialization/"))
+            }
+
+            sourceLink {
+                val gradleVersion = version.toString()
+
+                val gitVersion = if (!gradleVersion.contains("SNAPSHOT")) {
+                    gradleVersion
+                } else {
+                    "main"
+                }
+
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URL("$githubRepo/blob/$gitVersion/src/main/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
+
+    outputDirectory.set(buildDir.resolve("dokka"))
+    suppressInheritedMembers.set(true)
+
+    pluginsMapConfiguration.set(
+        mapOf(
+            "org.jetbrains.dokka.base.DokkaBase" to """
+                {
+                    "footerMessage": "$footerMsg",
+                    "customStyleSheets": [ "${file("dokka/custom.css")}" ]
+                }
+            """.trimIndent()
+        )
+    )
 }
