6b2a448838e9ed8857533618ea2adfeeb8af197d
Tobias <xn32@posteo.net>
2022-10-29 23:51:46 +0200

    Add workflow to generate a coverage report

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import java.net.URL
 plugins {
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
     id("org.jetbrains.dokka") version "1.7.20"
     `maven-publish`
     `java-library`
