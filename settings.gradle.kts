7b4e168c377566083fbd0676ee1b8171a40d6c13
Adam <897017+aSemy@users.noreply.github.com>
2022-11-29 17:00:59 +0100

    rm unnecessary repo config (this project doesn't have Kotlin Native... yet)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,31 +7,6 @@ dependencyResolutionManagement {
 
   repositories {
     mavenCentral()
-
-    // workaround for https://youtrack.jetbrains.com/issue/KT-51379
-    exclusiveContent {
-      forRepository {
-        ivy("https://download.jetbrains.com/kotlin/native/builds") {
-          name = "Kotlin Native"
-          patternLayout {
-            listOf(
-              "macos-x86_64",
-              "macos-aarch64",
-              "osx-x86_64",
-              "osx-aarch64",
-              "linux-x86_64",
-              "windows-x86_64",
-            ).forEach { os ->
-              listOf("dev", "releases").forEach { stage ->
-                artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
-              }
-            }
-          }
-          metadataSources { artifact() }
-        }
-      }
-      filter { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
-    }
   }
 
   pluginManagement {
